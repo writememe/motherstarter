5a4d0d4458130956d0861090c7a5e48e9a2efa2e
Daniel Teycheney <danielfjteycheney@gmail.com>
2020-10-01 21:39:26 +1000

    pyATS uplift, CSV and XLSX output.

--- import.py
+++ import.py
@@ -78,12 +78,58 @@ def to_nr_groups(env, df, output_dir=None):
     print(f"File output location: {nr_g_file.name}")
 
 
+def to_pyats(env, df, output_dir=None):
+    """"""
+    if output_dir is None:
+        output_dir = "outputs/pyats"
+    # Create entry directory and/or check that it exists
+    pl.Path(output_dir).mkdir(parents=True, exist_ok=True)
+    inv = dataframe_to_dict(df)
+    template = env.get_template("testbed.j2")
+    output = template.render(inventory=inv)
+    with open(f"{output_dir}/mother_starter_tb.yaml", "w+") as tb_file:
+        tb_file.write(output)
+    print(f"File output location: {tb_file.name}")
+
+
+def to_csv(df, output_dir=None):
+    """
+    """
+    if output_dir is None:
+        output_dir = "outputs/generic"
+    # Create entry directory and/or check that it exists
+    pl.Path(output_dir).mkdir(parents=True, exist_ok=True)
+    inv = dataframe_to_dict(df)
+    inv = df.from_dict(inv)
+    csv_file = f"{output_dir}/inventory.csv"
+    inv.to_csv(csv_file, index=False)
+    print(f"File output location: {csv_file}")
+
+
+def to_xlsx(df, output_dir=None):
+    """
+    """
+    if output_dir is None:
+        output_dir = "outputs/generic"
+    # Create entry directory and/or check that it exists
+    pl.Path(output_dir).mkdir(parents=True, exist_ok=True)
+    inv = dataframe_to_dict(df)
+    inv = df.from_dict(inv)
+    xlsx_file = f"{output_dir}/inventory.xlsx"
+    inv.to_csv(xlsx_file, index=False)
+    print(f"File output location: {xlsx_file}")
+
+
 def main():
     df = init_dataframe()
     group_df = init_grp_dataframe()
     env = prep_templates()
+    pyats_env = prep_templates(tmpl_dir="templates/outputs/pyats")
     to_nr_hosts(env, df)
     to_nr_groups(env=env, df=group_df)
+    to_csv(df)
+    to_xlsx(df)
+    to_pyats(env=pyats_env, df=df)
 
 
 main()
