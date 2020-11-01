ac37cdfd8fea8cbf833793cfe8adb759ca426df5
Daniel Teycheney <danielfjteycheney@gmail.com>
2020-11-01 21:05:55 +1100

    Uplifting test profiles

--- tests/e2e/test_click_inputs.py
+++ tests/e2e/test_click_inputs.py
@@ -29,7 +29,7 @@ def test_convert_default(runner):
     assert expected_source_dir in result.output
 
 
-def test_version(runner):
+def test_base_version(runner):
     result = runner.invoke(ms.cli, ["--version"])
     if result.exception:
         traceback.print_exception(*result.exc_info)  # noqa
@@ -38,13 +38,27 @@ def test_version(runner):
     assert expected in result.output
 
 
-def test_help(runner):
+def test_base_help(runner):
     result = runner.invoke(ms.cli, ["--help"])
     if result.exception:
         traceback.print_exception(*result.exc_info)  # noqa
     expected_usage = "Usage: cli [OPTIONS] COMMAND [ARGS]."
     expected_convert = (
         "convert  Convert source file(s) into network automation inventory outputs"
+    )
+    assert result.exit_code == 0
+    assert expected_usage in result.output
+    assert expected_convert in result.output
+
+
+def test_convert_help(runner):
+    result = runner.invoke(ms.convert, ["--help"])
+    if result.exception:
+        traceback.print_exception(*result.exc_info)  # noqa
+    expected_usage = "Usage: convert [OPTIONS]"
+    expected_convert = (
+        "Convert source file(s) into network automation inventory outputs based on"
+        "\n  multiple command-line inputs"
     )
     assert result.exit_code == 0
     assert expected_usage in result.output
@@ -91,7 +105,6 @@ def test_convert_source_type_bad(runner):
 
 def test_convert_source_dir_custom(runner):
     sd = "tests/test_data/core"
-    # sd = "motherstarter/inputs"
     result = runner.invoke(ms.convert, ["-sd", sd])
     if result.exception:
         traceback.print_exception(*result.exc_info)  # noqa
@@ -106,7 +119,6 @@ def test_convert_source_dir_custom(runner):
 
 def test_convert_source_dir_bad(runner):
     sd = "tests/test_data/core/bad"
-    # sd = "motherstarter/inputs"
     result = runner.invoke(ms.convert, ["-sd", sd])
     assert result.exit_code == 1
 
@@ -185,12 +197,136 @@ def test_convert_output_type_csv(runner):
     result = runner.invoke(ms.convert, ["-o", ot])
     expected_output_type = f"DEBUG - Output type is: {ot}"
     expected_output_inv_file = (
-        f"INFO - File output location: motherstarter/outputs/{ot}/inventory.csv"
+        f"INFO - File output location: motherstarter/outputs/{ot}/inventory.{ot}"
     )
     expected_output_groups_file = (
-        f"INFO - File output location: motherstarter/outputs/{ot}/groups.csv"
+        f"INFO - File output location: motherstarter/outputs/{ot}/groups.{ot}"
     )
     assert result.exit_code == 0
     assert expected_output_type in result.output
     assert expected_output_inv_file in result.output
     assert expected_output_groups_file in result.output
+
+
+def test_convert_output_type_json(runner):
+    ot = "json"
+    result = runner.invoke(ms.convert, ["-o", ot])
+    expected_output_type = f"DEBUG - Output type is: {ot}"
+    expected_output_inv_file = (
+        f"INFO - File output location: motherstarter/outputs/{ot}/inventory.{ot}"
+    )
+    expected_output_groups_file = (
+        f"INFO - File output location: motherstarter/outputs/{ot}/groups.{ot}"
+    )
+    assert result.exit_code == 0
+    assert expected_output_type in result.output
+    assert expected_output_inv_file in result.output
+    assert expected_output_groups_file in result.output
+
+
+def test_convert_output_type_xlsx(runner):
+    ot = "xlsx"
+    result = runner.invoke(ms.convert, ["-o", ot])
+    expected_output_type = f"DEBUG - Output type is: {ot}"
+    expected_output_inv_file = (
+        f"INFO - File output location: motherstarter/outputs/{ot}/inventory.{ot}"
+    )
+    expected_output_groups_file = (
+        f"INFO - File output location: motherstarter/outputs/{ot}/groups.{ot}"
+    )
+    assert result.exit_code == 0
+    assert expected_output_type in result.output
+    assert expected_output_inv_file in result.output
+    assert expected_output_groups_file in result.output
+
+
+def test_convert_output_type_nornir(runner):
+    ot = "nornir"
+    result = runner.invoke(ms.convert, ["-o", ot])
+    expected_output_type = f"DEBUG - Output type is: {ot}"
+    expected_output_inv_file = (
+        "INFO - File output location: motherstarter/outputs/nr/inventory/groups.yaml"
+    )
+    expected_output_groups_file = (
+        "INFO - File output location: motherstarter/outputs/nr/inventory/hosts.yaml"
+    )
+    assert result.exit_code == 0
+    assert expected_output_type in result.output
+    assert expected_output_inv_file in result.output
+    assert expected_output_groups_file in result.output
+
+
+def test_convert_output_type_pyats(runner):
+    ot = "pyats"
+    result = runner.invoke(ms.convert, ["-o", ot])
+    expected_output_type = f"DEBUG - Output type is: {ot}"
+    expected_output_inv_file = f"INFO - File output location: motherstarter/outputs/{ot}/mother_starter_tb.yaml"  # noqa
+    assert result.exit_code == 0
+    assert expected_output_type in result.output
+    assert expected_output_inv_file in result.output
+
+
+def test_convert_output_type_ansible(runner):
+    ot = "ansible"
+    result = runner.invoke(ms.convert, ["-o", ot])
+    expected_output_type = f"DEBUG - Output type is: {ot}"
+    expected_output_inv_file = (
+        f"INFO - File output location: motherstarter/outputs/{ot}/inventory/hosts"
+    )
+    assert result.exit_code == 0
+    assert expected_output_type in result.output
+    assert expected_output_inv_file in result.output
+
+
+def test_convert_output_type_all(runner):
+    ot = "all"
+    result = runner.invoke(ms.convert, ["-o", ot])
+    expected_output_type = f"DEBUG - Output type is: {ot}"
+    expected_output_inv_csv_file = (
+        "INFO - File output location: motherstarter/outputs/csv/inventory.csv"
+    )
+    expected_output_groups_csv_file = (
+        "INFO - File output location: motherstarter/outputs/csv/groups.csv"
+    )
+    expected_output_inv_json_file = (
+        "INFO - File output location: motherstarter/outputs/json/inventory.json"
+    )
+    expected_output_groups_json_file = (
+        "INFO - File output location: motherstarter/outputs/json/groups.json"
+    )
+    expected_output_inv_xlsx_file = (
+        "INFO - File output location: motherstarter/outputs/xlsx/inventory.xlsx"
+    )
+    expected_output_groups_xlsx_file = (
+        "INFO - File output location: motherstarter/outputs/xlsx/groups.xlsx"
+    )
+    expected_output_inv_nornir_file = (
+        "INFO - File output location: motherstarter/outputs/nr/inventory/groups.yaml"
+    )
+    expected_output_groups_nornir_file = (
+        "INFO - File output location: motherstarter/outputs/nr/inventory/hosts.yaml"
+    )
+    expected_output_pyats_inv_file = "INFO - File output location: motherstarter/outputs/pyats/mother_starter_tb.yaml"  # noqa
+    expected_output_inv_ansible_file = (
+        "INFO - File output location: motherstarter/outputs/ansible/inventory/hosts"
+    )
+    assert result.exit_code == 0
+    assert expected_output_type in result.output
+    assert expected_output_inv_csv_file in result.output
+    assert expected_output_groups_csv_file in result.output
+    assert expected_output_inv_json_file in result.output
+    assert expected_output_groups_json_file in result.output
+    assert expected_output_inv_xlsx_file in result.output
+    assert expected_output_groups_xlsx_file in result.output
+    assert expected_output_inv_nornir_file in result.output
+    assert expected_output_groups_nornir_file in result.output
+    assert expected_output_pyats_inv_file in result.output
+    assert expected_output_inv_ansible_file in result.output
+
+
+def test_convert_output_type_bad(runner):
+    ot = "bad"
+    result = runner.invoke(ms.convert, ["-o", ot])
+    expected_block = f"Error: Invalid value for '--output-type' / '-o': invalid choice: {ot}. (choose from all, ansible, csv, json, nornir, pyats, xlsx)"  # noqa
+    assert result.exit_code == 2
+    assert expected_block in result.output
