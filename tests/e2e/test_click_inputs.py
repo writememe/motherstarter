324d06af0601442a61cf252d50613ff3eb43defe
Daniel Teycheney <danielfjteycheney@gmail.com>
2020-11-01 15:06:52 +1100

    Tests updated

--- tests/e2e/test_click_inputs.py
+++ tests/e2e/test_click_inputs.py
@@ -6,6 +6,7 @@ This is where I am testing inputs via the CLI
 from click.testing import CliRunner
 import pytest
 from motherstarter import motherstarter as ms
+import traceback
 
 
 @pytest.fixture(scope="module")
@@ -13,7 +14,7 @@ def runner():
     return CliRunner()
 
 
-def test_default_convert(runner):
+def test_convert_default(runner):
     result = runner.invoke(ms.cli, ["convert"])
     if result.exception:
         traceback.print_exception(*result.exc_info)  # noqa
@@ -37,7 +38,20 @@ def test_version(runner):
     assert expected in result.output
 
 
-def test_named_convert_csv(runner):
+def test_help(runner):
+    result = runner.invoke(ms.cli, ["--help"])
+    if result.exception:
+        traceback.print_exception(*result.exc_info)  # noqa
+    expected_usage = "Usage: cli [OPTIONS] COMMAND [ARGS]."
+    expected_convert = (
+        "convert  Convert source file(s) into network automation inventory outputs"
+    )
+    assert result.exit_code == 0
+    assert expected_usage in result.output
+    assert expected_convert in result.output
+
+
+def test_convert_source_type_csv(runner):
     st = "csv"
     result = runner.invoke(ms.convert, ["-st", st])
     if result.exception:
@@ -47,7 +61,7 @@ def test_named_convert_csv(runner):
     assert expected_block in result.output
 
 
-def test_named_convert_json(runner):
+def test_convert_source_type_json(runner):
     st = "json"
     result = runner.invoke(ms.convert, ["-st", st])
     if result.exception:
@@ -57,7 +71,7 @@ def test_named_convert_json(runner):
     assert expected_block in result.output
 
 
-def test_named_convert_xlsx(runner):
+def test_convert_source_type_xlsx(runner):
     st = "xlsx"
     result = runner.invoke(ms.convert, ["-st", st])
     if result.exception:
@@ -66,13 +80,117 @@ def test_named_convert_xlsx(runner):
     assert result.exit_code == 0
     assert expected_block in result.output
 
-def test_named_convert_bad(runner):
+
+def test_convert_source_type_bad(runner):
     st = "bad"
     result = runner.invoke(ms.convert, ["-st", st])
-    if result.exception:
-        print(result.output)
-        print(result.exception)
-        print(result.exc_info)
-    expected_block = f"Error: Invalid value for '--source-type' / '-st': invalid choice: {st}. (choose from csv, json, xlsx)"
+    expected_block = f"Error: Invalid value for '--source-type' / '-st': invalid choice: {st}. (choose from csv, json, xlsx)"  # noqa
     assert result.exit_code == 2
     assert expected_block in result.output
+
+
+def test_convert_source_dir_custom(runner):
+    sd = "tests/test_data/core"
+    # sd = "motherstarter/inputs"
+    result = runner.invoke(ms.convert, ["-sd", sd])
+    if result.exception:
+        traceback.print_exception(*result.exc_info)  # noqa
+    expected_source_type = "DEBUG - Inventory source type is json"
+    expected_output_type = "DEBUG - Output type is: all"
+    expected_source_dir = f"DEBUG - Source directory is: {sd}"
+    assert result.exit_code == 0
+    assert expected_source_type in result.output
+    assert expected_output_type in result.output
+    assert expected_source_dir in result.output
+
+
+def test_convert_source_dir_bad(runner):
+    sd = "tests/test_data/core/bad"
+    # sd = "motherstarter/inputs"
+    result = runner.invoke(ms.convert, ["-sd", sd])
+    assert result.exit_code == 1
+
+
+def test_convert_log_level_debug(runner):
+    ll = "debug"
+    result = runner.invoke(ms.convert, ["-l", ll])
+    if result.exception:
+        traceback.print_exception(*result.exc_info)  # noqa
+    expected_source_type = "DEBUG - Inventory source type is json"
+    expected_output_type = "DEBUG - Output type is: all"
+    assert result.exit_code == 0
+    assert expected_source_type in result.output
+    assert expected_output_type in result.output
+    assert "ERROR -" not in result.output
+    assert "CRITICAL -" not in result.output
+
+
+def test_convert_log_level_info(runner):
+    ll = "info"
+    result = runner.invoke(ms.convert, ["-l", ll])
+    if result.exception:
+        traceback.print_exception(*result.exc_info)  # noqa
+    assert result.exit_code == 0
+    assert "DEBUG -" not in result.output
+    assert "ERROR -" not in result.output
+    assert "CRITICAL -" not in result.output
+
+
+def test_convert_log_level_warning(runner):
+    ll = "warning"
+    result = runner.invoke(ms.convert, ["-l", ll])
+    if result.exception:
+        traceback.print_exception(*result.exc_info)  # noqa
+    assert result.exit_code == 0
+    assert "DEBUG -" not in result.output
+    assert "INFO -" not in result.output
+    assert "ERROR -" not in result.output
+    assert "CRITICAL -" not in result.output
+
+
+def test_convert_log_level_error(runner):
+    ll = "error"
+    result = runner.invoke(ms.convert, ["-l", ll])
+    if result.exception:
+        traceback.print_exception(*result.exc_info)  # noqa
+    assert result.exit_code == 0
+    assert "DEBUG -" not in result.output
+    assert "INFO -" not in result.output
+    assert "WARNING -" not in result.output
+    assert "CRITICAL -" not in result.output
+
+
+def test_convert_log_level_critical(runner):
+    ll = "critical"
+    result = runner.invoke(ms.convert, ["-l", ll])
+    if result.exception:
+        traceback.print_exception(*result.exc_info)  # noqa
+    assert result.exit_code == 0
+    assert "DEBUG -" not in result.output
+    assert "INFO -" not in result.output
+    assert "WARNING -" not in result.output
+    assert "ERROR -" not in result.output
+
+
+def test_convert_log_level_bad(runner):
+    ll = "bad"
+    result = runner.invoke(ms.convert, ["-l", ll])
+    expected_block = f"Error: Invalid value for '--log-level' / '-l': invalid choice: {ll}. (choose from debug, info, warning, error, critical)"  # noqa
+    assert result.exit_code == 2
+    assert expected_block in result.output
+
+
+def test_convert_output_type_csv(runner):
+    ot = "csv"
+    result = runner.invoke(ms.convert, ["-o", ot])
+    expected_output_type = f"DEBUG - Output type is: {ot}"
+    expected_output_inv_file = (
+        f"INFO - File output location: motherstarter/outputs/{ot}/inventory.csv"
+    )
+    expected_output_groups_file = (
+        f"INFO - File output location: motherstarter/outputs/{ot}/groups.csv"
+    )
+    assert result.exit_code == 0
+    assert expected_output_type in result.output
+    assert expected_output_inv_file in result.output
+    assert expected_output_groups_file in result.output
--- tests/e2e/test_inputs.py
+++ tests/e2e/test_inputs.py
@@ -1,29 +1,29 @@
 # Import modules
-import pprint
-import pytest
-import motherstarter
-import json
+# import pprint
+# import pytest
+# import motherstarter
+# import json
 
-# Set indentation on pretty print
-pp = pprint.PrettyPrinter(indent=2)
+# # Set indentation on pretty print
+# pp = pprint.PrettyPrinter(indent=2)
 
-# Params for testing
-logger = "DEBUG"
-source_type = "json"
-output_type = "all"
-source_dir = "../test_data/core"
+# # Params for testing
+# logger = "DEBUG"
+# source_type = "json"
+# output_type = "all"
+# source_dir = "../test_data/core"
 
 
-# Add pytest mark
-@pytest.mark.inputs
-def inventory_json(logger, source_type, output_type, source_dir):
-    result = motherstarter.main.from_parent(
-        logger=logger,
-        source_type=source_type,
-        output_type=output_type,
-        source_dir=source_dir,
-    )
-    with open("../../motherstarter/outputs/json/inventory.json") as f:
-        inv_data = json.load(f)
-    assert inv_data[0]["name"] == "lab-csr-01.lab.dfjt.local2"
-    print(result)
+# # Add pytest mark
+# @pytest.mark.inputs
+# def inventory_json(logger, source_type, output_type, source_dir):
+#     result = motherstarter.main.from_parent(
+#         logger=logger,
+#         source_type=source_type,
+#         output_type=output_type,
+#         source_dir=source_dir,
+#     )
+#     with open("../../motherstarter/outputs/json/inventory.json") as f:
+#         inv_data = json.load(f)
+#     assert inv_data[0]["name"] == "lab-csr-01.lab.dfjt.local2"
+#     print(result)
