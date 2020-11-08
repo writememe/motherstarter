f3fb687074d04777eecbdb8caad83ab7ea864c68
Daniel Teycheney <danielfjteycheney@gmail.com>
2020-11-08 21:35:40 +1100

    Final major development before beta release (#41)
    * Code relocation
    
    * Folder relocation
    
    * Removed version 3.9 for now.
    
    * Attempting to optimise Makefile
    
    * Pinning requirements file
    
    * Fixing outputs and requirements file.
    
    * Putting argparse into a function.
    
    * Fixing argparse function
    
    * Click uplift.
    
    * Updating readme.
    
    * Adding ansible as an output option.
    
    * Updated readme and output files
    
    * Black file reformat,
    
    * Docstrings refactored.
    
    * Fixing docstrings for consistency.
    
    * Fixing documentation
    
    * Exposing source_dir functionality.
    
    * Adding setup file and entrypoint.
    
    * Add entrypoint for motherstarter app.
    
    * Refactor logging to remove global variable hack
    
    * Adding `install_requires` to install off requirements.txt
    
    * Feature/path repoint (#23)
    
    * Updating documentation
    
    * Update README link.:
    
    * Update README link.:
    
    * Update README table.
    
    * Update README table.
    
    * Updating documentation.
    
    * Fixing typo, bold into rows
    
    * Fixing typo
    
    * Feature/path repoint (#24)
    
    * Updating documentation
    
    * Update README link.:
    
    * Update README link.:
    
    * Update README table.
    
    * Update README table.
    
    * Updating documentation.
    
    * Fixing typo, bold into rows
    
    * Fixing typo
    
    * Adding 3.9 support (#25)
    
    * Adding version 3.9 support.
    
    * Adding 3.9 to README page
    
    * Flatten input folder structure (#26)
    
    * Simplify input folder structure.
    
    * Removal of old argparse function.
    
    * Fixing Excel input issue.
    
    * Adding mypy to reqs
    
    * Feature/pytest uplift (#28)
    
    * Adding testing structure.
    
    * Adding test structure.
    
    * Got testing structure in place
    
    * Tests updated
    
    * Uplifting test profiles
    
    * Adding pytests.
    
    * Fix workflow file.
    
    * Bump pandas from 1.1.3 to 1.1.4 (#30)
    
    Bumps [pandas](https://github.com/pandas-dev/pandas) from 1.1.3 to 1.1.4.
    - [Release notes](https://github.com/pandas-dev/pandas/releases)
    - [Changelog](https://github.com/pandas-dev/pandas/blob/master/RELEASE.md)
    - [Commits](https://github.com/pandas-dev/pandas/compare/v1.1.3...v1.1.4)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>
    
    * Adding dependabot. (#31)
    
    * Update doco and test progress docs. (#33)
    
    * Update doco and test progress docs.
    
    * Trialling code coverage upload.
    
    * Adding pre-reqs file.
    
    * Changing codecov version to 1.0.14.
    
    * Toggle workflow settings.
    
    * Trialling workflow settings.
    
    * Adding codecov documentation
    
    * Feature/pytest doco (#35)
    
    * Update doco and test progress docs.
    
    * Trialling code coverage upload.
    
    * Adding pre-reqs file.
    
    * Changing codecov version to 1.0.14.
    
    * Toggle workflow settings.
    
    * Trialling workflow settings.
    
    * Adding codecov documentation
    
    * Adding unit tests.
    
    * Feature/version repoint (#37)
    
    * Fixing version pointing, adding 3.9 to setup file.
    
    * Black reformat.
    
    * License fix and cal ver versioning (#38)
    
    * Adding license to setup.py file
    
    * Changing versioning to calver.
    
    * Template dir customisation (#39)
    
    * Template dir refactor.
    
    * Updating documentation.
    
    * Feature/nox uplift (#40)
    
    * Trialling nox.
    
    * Adding nox to requirements files.
    
    * Fixing noxfile syntax
    
    * Skipping nox
    
    * Fixing syntax
    
    * Fixing motherstarter target
    
    * Nox uplift and bandit refactor
    
    * Using noxfile.
    
    * Makefile uplift, nox refactor
    
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>

---
+++ noxfile.py
@@ -0,0 +1,146 @@
+"""
+motherstarter noxfile
+"""
+# Import modules
+import nox
+
+
+@nox.session(python=["3.6", "3.7", "3.8", "3.9"])
+def lint(session):
+    """
+    Run all linting tests.
+    Nox run black, pylama, yamllint and bandit
+
+    Args:
+        session: nox session
+
+    Returns:
+        N/A
+
+    Raises:
+        N/A
+    """
+    session.install("-r", "requirements.txt")
+    session.run("black", ".", "--check")
+    session.run("pylama", ".")
+    session.run("yamllint", ".")
+    session.run(
+        "bandit",
+        "-v",
+        "--exclude",
+        "./venv",
+        "--recursive",
+        "--format",
+        "json",
+        "motherstarter/",
+        "--verbose",
+        "-s",
+        "B101",
+    )
+
+
+@nox.session(python=["3.6", "3.7", "3.8", "3.9"])
+def black(session):
+    """
+    Nox run black
+
+    Args:
+        session: nox session
+
+    Returns:
+        N/A
+
+    Raises:
+        N/A
+
+    """
+    session.install("-r", "requirements.txt")
+    session.run("black", ".", "--check")
+
+
+@nox.session(python=["3.6", "3.7", "3.8", "3.9"])
+def pylama(session):
+    """
+    Nox run pylama
+
+    Args:
+        session: nox session
+
+    Returns:
+        N/A
+
+    Raises:
+        N/A
+
+    """
+    session.install("-r", "requirements.txt")
+    session.run("pylama", ".")
+
+
+@nox.session(python=["3.6", "3.7", "3.8", "3.9"])
+def yamllint(session):
+    """
+    Nox run yamllint
+
+    Args:
+        session: nox session
+
+    Returns:
+        N/A
+
+    Raises:
+        N/A
+
+    """
+    session.install("-r", "requirements.txt")
+    session.run("yamllint", ".")
+
+
+@nox.session(python=["3.6", "3.7", "3.8", "3.9"])
+def bandit(session):
+    """
+    Nox run bandit
+
+    Args:
+        session: nox session
+
+    Returns:
+        N/A
+
+    Raises:
+        N/A
+
+    """
+    session.install("-r", "requirements.txt")
+    session.run(
+        "bandit",
+        "-v",
+        "--exclude",
+        "./venv",
+        "--recursive",
+        "--format",
+        "json",
+        "motherstarter/",
+        "--verbose",
+        "-s",
+        "B101",
+    )
+
+
+@nox.session(python=["3.6", "3.7", "3.8", "3.9"])
+def tests(session):
+    """
+    Nox run tests using pytest
+
+    Args:
+        session: nox session
+
+    Returns:
+        N/A
+
+    Raises:
+        N/A
+
+    """
+    session.install("-r", "requirements.txt")
+    session.run("pytest", "--cov=./", "--cov-report=xml")
