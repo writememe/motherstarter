f029d75c70d62ebaeb6d375a49432ddbcbb745b6
Daniel Teycheney <danielfjteycheney@gmail.com>
2020-11-08 12:17:34 +1100

    Template dir customisation (#39)
    * Template dir refactor.
    
    * Updating documentation.

--- tests/unit/test_source_type.py
+++ tests/unit/test_source_type.py
@@ -19,7 +19,7 @@ def test_init_inventory_bad():
     logger = ms.init_logger(log_level="CRITICAL", log_name="unit-tests.log")
     # Initialise the init_inventory function with a bad source_type
     df = ms.init_inventory(
-        logger=logger, source_dir="tests/test_data/core", source_type="bad"
+        logger=logger, source_dir="tests/test_data/inputs/core", source_type="bad"
     )
     assert df is None
 
@@ -29,6 +29,6 @@ def test_init_groups_bad():
     logger = ms.init_logger(log_level="CRITICAL", log_name="unit-tests.log")
     # Initialise the init_groups function with a bad source_type
     df = ms.init_groups(
-        logger=logger, source_dir="tests/test_data/core", source_type="bad"
+        logger=logger, source_dir="tests/test_data/inputs/core", source_type="bad"
     )
     assert df is None
